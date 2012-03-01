a788211f5f7e1fe61cd8127564f98c13c05f5b16
sundapeng2008 <sundapeng2008@c0500102-add6-e572-5948-91ab29ba1906>
2012-03-01 03:20:13 +0000

    使用Spring管理依赖

--- trunk/index/src/main/java/com/sundp/handle/DocHandle.java
+++ trunk/index/src/main/java/com/sundp/handle/DocHandle.java
@@ -1,7 +1,6 @@
 package com.sundp.handle;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
